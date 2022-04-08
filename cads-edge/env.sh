b19a2a51166e21da1acdadfe43ce1d7d2e657296
cadsdev <cadsdev@cads-dev.localdomain>
2022-04-08 21:24:53 +1000

    Impove build

--- cads-edge/env.sh
+++ cads-edge/env.sh
@@ -2,3 +2,9 @@
 
 export CXX=`which g++-10`
 export CC=`which gcc-10`
+export BOOST_LIBRARYDIR=/usr/lib
+export BOOST_INCLUDEDIR=/usr/include/boost
+
+if [ -d "$HOME/.local/bin" ] ; then
+	PATH="$HOME/.local/bin:$PATH"
+fi
