5bb87b1369f32b71fadb4875914242d88e7ed671
Jeff <jeff@hexagonhill.dev>
2022-04-28 20:47:25 +0930

    Use gcc 11

--- cads-edge/env.sh
+++ cads-edge/env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 
-export CXX=`which g++-10`
-export CC=`which gcc-10`
+export CXX=`which g++-11`
+export CC=`which gcc-11`
 export BOOST_LIBRARYDIR=/home/macro/cads/boost/usr/lib
 export BOOST_INCLUDEDIR=/home/macro/cads/boost/usr/include/boost
 LD_LIBRARY_PATH=$BOOST_LIBRARYDIR
