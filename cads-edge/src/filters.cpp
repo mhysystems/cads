38a08639b46ee73bf0c4bbe21e39548ec15b8a0a
me <me@me.localdomain>
2022-08-07 10:09:06 +1000

    disable max height clipping

--- cads-edge/src/filters.cpp
+++ cads-edge/src/filters.cpp
@@ -43,7 +43,7 @@ namespace cads
     auto prev_value_it = sr::find_if(z,[](z_element a){ return !NaN<z_element>::isnan(a);}); 
     z_element prev_value = prev_value_it != z.end() ? *prev_value_it : NaN<z_element>::value; 
     
-    int mid = (int)z.size() / 2;
+    int mid = (int)(z.size() / 2);
 
     for(auto&& e : z | sr::views::take(mid)) {
       if(!NaN<z_element>::isnan(e)) {
@@ -69,7 +69,7 @@ namespace cads
     for(auto &e : z) {
       e += z_off;
       if(e < 0) e = 0;
-      if(e > z_max) e = z_max;
+      //if(e > z_max) e = z_max;
     }
   }
 
