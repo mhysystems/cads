c49369ff8bbbe3eba8685b03ecc4f57298f9eccc
Paul McAlpine <paul.mcalpine@mhysystems.com>
2022-10-17 11:35:31 +0800

    Store hashes and fix external paths

--- buildroot-external/package/hardware-nvidia/hardware-nvidia.mk
+++ buildroot-external/package/hardware-nvidia/hardware-nvidia.mk
@@ -7,4 +7,4 @@
 # $(BUILD_DIR)/hardware/nvidia maps to Linux_for_Tegra/sources/hardware/nvidia
 NV_BUILD_KERNEL_DTS_ROOT = $(BUILD_DIR)/hardware/nvidia
 
-include $(sort $(wildcard package/hardware-nvidia/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_DEFAULT_PATH)/package/hardware-nvidia/*/*.mk))
