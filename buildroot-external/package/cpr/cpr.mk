7a046fa65f14beef102157f544ddadbfaa9567e4
Paul McAlpine <paul.mcalpine@mhysystems.com>
2022-10-18 21:04:34 +0800

    Build cpr (libcpr) 1.9.2

--- buildroot-external/package/cpr/cpr.mk
+++ buildroot-external/package/cpr/cpr.mk
@@ -4,7 +4,10 @@
 #
 ################################################################################
 
-CPR_VERSION = 1.8.1
-CPR_SITE = $(call github,libcrp,cpr,$(CPR_VERSION))
+CPR_VERSION = 1.9.2
+CPR_SITE = $(call github,libcpr,cpr,$(CPR_VERSION))
+CPR_LICENSE = MIT
+CPR_INSTALL_STAGING = YES
+CPR_CONF_OPTS = -DCPR_BUILD_TESTS=OFF -DCPR_FORCE_USE_SYSTEM_CURL=ON
 
 $(eval $(cmake-package))
