36a7478fd4c2b9f16f64a5b016dde566b04d1e0a
Paul McAlpine <paul.mcalpine@mhysystems.com>
2022-10-28 09:45:40 +0800

    Fix dependencies and getty creation

--- cads-os/buildroot-external/package/cads-support/cads-support.mk
+++ cads-os/buildroot-external/package/cads-support/cads-support.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-define CADS_SUPPORT_INSTALL_TARGET_CMDS
-	ln -s /lib/systemd/system/getty@.service $(TARGET_DIR)/etc/systemd/system/getty.target.wants/getty@tty1.service
+define CADS_SUPPORT_INSTALL_INIT_SYSTEMD
+	mkdir -p $(TARGET_DIR)/lib/systemd/system/getty.target.wants
+	ln -snf ../getty@.service $(TARGET_DIR)/lib/systemd/system/getty.target.wants/getty@tty1.service
 endef
 
 $(eval $(generic-package))
