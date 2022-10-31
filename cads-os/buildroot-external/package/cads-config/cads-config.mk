29bf72d2c341f7a5df080a124e5266ce3fc789d3
Paul McAlpine <paul.mcalpine@mhysystems.com>
2022-11-01 00:58:06 +0800

    Set up network bridge in the systemd way

--- cads-os/buildroot-external/package/cads-config/cads-config.mk
+++ cads-os/buildroot-external/package/cads-config/cads-config.mk
@@ -9,10 +9,18 @@ define CADS_CONFIG_INSTALL_INIT_SYSTEMD
 	mkdir -p $(TARGET_DIR)/lib/systemd/system/getty.target.wants
 	ln -snf ../getty@.service $(TARGET_DIR)/lib/systemd/system/getty.target.wants/getty@tty1.service
 
-	# networks
-	$(INSTALL) -D -m 0644 $(CADS_CONFIG_PKGDIR)10-modem.link $(TARGET_DIR)/lib/systemd/network/10-modem.link
-	$(INSTALL) -D -m 0644 $(CADS_CONFIG_PKGDIR)10-wired.link $(TARGET_DIR)/lib/systemd/network/10-wired.link
-	$(INSTALL) -D -m 0644 $(CADS_CONFIG_PKGDIR)20-wired.network $(TARGET_DIR)/lib/systemd/network/20-wired.network
+	# modem
+	$(INSTALL) -D -m 0644 $(CADS_CONFIG_PKGDIR)/10-modem.link $(TARGET_DIR)/lib/systemd/network/10-modem.link
+
+	# ethernet
+	$(INSTALL) -D -m 0644 $(CADS_CONFIG_PKGDIR)/20-wired.link $(TARGET_DIR)/lib/systemd/network/20-wired.link
+	$(INSTALL) -D -m 0644 $(CADS_CONFIG_PKGDIR)/20-wired.network $(TARGET_DIR)/lib/systemd/network/20-wired.network
+
+	# usb ethernet
+	$(INSTALL) -D -m 0644 $(CADS_CONFIG_PKGDIR)/30-rndis0.network $(TARGET_DIR)/lib/systemd/network/30-rndis0.network
+	$(INSTALL) -D -m 0644 $(CADS_CONFIG_PKGDIR)/30-usb0.network $(TARGET_DIR)/lib/systemd/network/30-usb0.network
+	$(INSTALL) -D -m 0644 $(CADS_CONFIG_PKGDIR)/40-l4tbr0.netdev $(TARGET_DIR)/lib/systemd/network/40-l4tbr0.netdev
+	$(INSTALL) -D -m 0644 $(CADS_CONFIG_PKGDIR)/40-l4tbr0.network $(TARGET_DIR)/lib/systemd/network/40-l4tbr0.network
 endef
 
 $(eval $(generic-package))
--- cads-os/buildroot-external/package/tegra210-usb-gadget/tegra210-usb-gadget.mk
+++ cads-os/buildroot-external/package/tegra210-usb-gadget/tegra210-usb-gadget.mk
@@ -4,9 +4,12 @@
 #
 ################################################################################
 
-TEGRA210_USB_GADGET_DEPENDENCIES += systemd
+TEGRA210_USB_GADGET_DEPENDENCIES += systemd tegra210
 
 define TEGRA210_USB_GADGET_INSTALL_COMMON
+	$(RM) $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/nv-l4t-usb-device-mode.service
+	$(RM) $(TARGET_DIR)/etc/systemd/system/nv-l4t-usb-device-mode.service
+	$(RM) $(TARGET_DIR)/etc/systemd/system/nv-l4t-usb-device-mode-runtime.service
 	$(INSTALL) -D -m 0755 $(TEGRA210_USB_GADGET_PKGDIR)/usb-gadget-start.sh \
 		$(TARGET_DIR)/usr/libexec/usb-gadget/usb-gadget-start.sh
 	$(INSTALL) -D -m 0755 $(TEGRA210_USB_GADGET_PKGDIR)/usb-gadget-stop.sh \
@@ -17,8 +20,6 @@ define TEGRA210_USB_GADGET_INSTALL_INIT_SYSTEMD
 	$(TEGRA210_USB_GADGET_INSTALL_COMMON)
 	$(INSTALL) -D -m 0644 $(TEGRA210_USB_GADGET_PKGDIR)/usb-gadget.service \
 		$(TARGET_DIR)/lib/systemd/system/usb-gadget.service
-	$(INSTALL) -D -m 0644 $(TEGRA210_USB_GADGET_PKGDIR)/20-usb-gadget.network \
-		$(TARGET_DIR)/lib/systemd/network/20-usb-gadget.network
 endef
 
 $(eval $(generic-package))
