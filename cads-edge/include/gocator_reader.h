16dbb318baa57b2aee3dfb264116e764b0b90f12
Jeff <jeff@hexagonhill.dev>
2023-02-17 23:36:29 +1100

    c++ threads alternative instead of gocator

--- cads-edge/include/gocator_reader.h
+++ cads-edge/include/gocator_reader.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <thread>
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wall"
@@ -33,16 +34,21 @@ protected:
   std::atomic<k64s> m_yOffset = 0;
   std::atomic<size_t> m_buffer_size_warning = 4096;
   std::atomic<bool> m_sent_final_msg = false;
+  std::jthread m_thread;
   
 	static kStatus OnData(kPointer context, GoSensor sensor, GoDataSet dataset);
   static kStatus OnSystem(kPointer context, GoSystem system, GoDataSet data);
 	virtual kStatus OnData(GoSensor sensor, GoDataSet dataset);
   virtual kStatus OnSystem(GoSystem system, GoDataSet dataset);
 
+  void OnData_thread();
+
 public:
 
 	void Start();
 	void Stop();
+	void Start_thread();
+	void Stop_thread();
   void Log();
 	GocatorReader(moodycamel::BlockingReaderWriterQueue<cads::msg>&, std::string ip_add = "");
   GocatorReader(moodycamel::BlockingReaderWriterQueue<cads::msg>&, bool, std::string ip_add = "");
