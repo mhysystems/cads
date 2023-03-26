1763d77ce8de370b04dad2e481b3f1fc4a07834d
Jeff <jeff@hexagonhill.dev>
2023-03-26 15:04:21 +1100

    clean up left over files

--- cads-edge/src/cads.cpp
+++ cads-edge/src/cads.cpp
@@ -556,6 +556,8 @@ namespace cads
         break;
       }
     }
+    
+    scan_upload_fifo.enqueue({msgid::finished,0});
   }
 
   void generate_signal()
--- cads-edge/src/save_send_thread.cpp
+++ cads-edge/src/save_send_thread.cpp
@@ -37,104 +37,6 @@ namespace {
 
 namespace cads
 {
-  coro<long, std::tuple<long, double>, 1> daily_upload_coro(long read_revid)
-  {
- 
-    using namespace date;
-    using namespace std;
- 
-    auto daily_upload = global_config["daily_upload"].get<bool>();
-    auto program_state_db_name = global_config["state_db_name"].get<std::string>();
-
-    auto write_revid = read_revid;
-
-    std::future<std::invoke_result_t<decltype(http_post_whole_belt), int, int, int>> fut;
-    bool terminate = false;
-    long idx = 0;
-    double belt_length = 0;
-    std::tuple<long, double> args;
-
-    enum state_t
-    {
-      pre_upload,
-      uploading,
-      no_shedule
-    };
-
-    auto state = daily_upload ? pre_upload : no_shedule;
-    auto next_upload_date = fetch_daily_upload(program_state_db_name);
-
-    for (; !terminate;)
-    {
-      std::tie(args, terminate) =  co_yield write_revid;
-      std::tie(idx, belt_length) = args;
-
-      if (terminate)
-        continue;
-
-      for (auto process = true; process;)
-      {
-
-        switch (state)
-        {
-        case no_shedule:
-        {
-          fut = std::async(http_post_whole_belt, read_revid, idx, 0);
-          write_revid++;
-          spdlog::get("cads")->info("Posting a belt. Writing data to revid: {}", write_revid);
-          state = uploading;
-          break;
-        }
-        case pre_upload:
-        {
-          auto now = date::utc_clock::now();
-  
-          if (now > next_upload_date)
-          {
-
-              fut = std::async(http_post_whole_belt, read_revid, idx, 0);
-              write_revid++;
-              state = uploading;
-              spdlog::get("cads")->info("Posting a belt. Writing data to revid: {}. Reading data from revid: {}", write_revid, read_revid);
-
-          }
-          else
-          {
-            process = false;
-          }
-          break;
-        }
-        case uploading:
-        {
-          if (fut.wait_for(std::chrono::seconds(0)) == std::future_status::ready)
-          {
-            auto [time,err] = fut.get();
-            read_revid = write_revid;
-            if (err)
-            {
-              write_revid++;
-              state = daily_upload ? pre_upload : no_shedule;
-            }
-            else
-            {
-              write_revid--;
-              state = daily_upload ? pre_upload : no_shedule;
-              next_upload_date += std::chrono::days(1);
-            }
-            spdlog::get("cads")->info("Belt upload thread finished. Writing data to revid: {}", write_revid);
-          }
-          else
-          {
-            process = false;
-          }
-          break;
-        }
-        }
-      }    
-    }
-
-  }
-
   void save_send_thread(BlockingReaderWriterQueue<msg> &profile_fifo, BlockingReaderWriterQueue<msg> &next)
   {
     namespace sml = boost::sml;
@@ -145,16 +47,14 @@ namespace cads
     struct global_t
     {
       cads::coro<int, std::tuple<int, int, cads::profile>, 1> store_profile = store_profile_coro();
-      coro<int, double,1> store_last_y = store_last_y_coro();
-      coro<long, std::tuple<long, double>, 1> daily_upload = daily_upload_coro(0);
-      coro<int, z_type, 1> store_scan;
-      long revid = 0;
       long idx = 0;
+      coro<int, double,1> store_last_y = store_last_y_coro();
+      coro<int, z_type, 1> store_scan;
       GocatorProperties gocator_properties;
       date::utc_clock::time_point scan_begin;
       BlockingReaderWriterQueue<msg> & cps;
 
-      global_t(BlockingReaderWriterQueue<msg> &m, date::utc_clock::time_point sb, std::string s) : cps(m), store_scan(store_scan_coro(s)), scan_begin(sb){};
+      global_t(BlockingReaderWriterQueue<msg> &m, date::utc_clock::time_point sb, std::string s) : store_scan(store_scan_coro(s)), scan_begin(sb), cps(m){};
 
     } global(next,scan_begin,scan_filename_init);
 
@@ -168,33 +68,23 @@ namespace cads
 
         const auto store_action = [](global_t &global, const scan_t &e)
         {
-          auto [co_end, s_err] = global.store_profile.resume({global.revid, global.idx, e.value});
+          global.store_profile.resume({0, global.idx++, e.value});
           global.store_scan.resume(e.value.z);
- 
-          if (s_err == 101)
-          {
-            global.idx++;
-          }
-          else
-          {
-            // error
-          }
-
           global.store_last_y.resume(e.value.y);
         };
 
         const auto complete_belt_action = [](global_t &global, const complete_belt_t &e)
         {
-          bool terminate = false;
-          //std::tie(terminate, global.revid) = global.daily_upload.resume({global.idx, e.value});
           global.idx = 0;
-          
+
           auto scan_end = date::utc_clock::now();
           auto scan_filename = fmt::format("scan-{}.sqlite",global.scan_begin.time_since_epoch().count());
           store_scan_gocator(get_gocator_subset(global.gocator_properties),scan_filename);
           store_scan_properties({global.scan_begin,scan_end},scan_filename);
+          
           store_scan_state(scan_filename);          
           global.cps.enqueue({msgid::complete_belt, 0});
+          
           auto new_scan_filename = fmt::format("scan-{}.sqlite",scan_end.time_since_epoch().count());
           global.store_scan = store_scan_coro(new_scan_filename);
           global.scan_begin = scan_end;
@@ -204,8 +94,6 @@ namespace cads
         const auto reset_globals_action = [](global_t &global)
         {
           global.idx = 0;
-          global.revid = 0;
-
           global.store_scan.terminate();
           auto scan_filename = fmt::format("scan-{}.sqlite",global.scan_begin.time_since_epoch().count());
           std::filesystem::remove(scan_filename);
@@ -235,8 +123,6 @@ namespace cads
 
     cads::msg m;
     sml::sm<transitions> sm{global};
-    auto start = std::chrono::high_resolution_clock::now();
-    int64_t cnt = 0;
 
     for (auto loop = true; loop;)
     {
@@ -267,15 +153,9 @@ namespace cads
       }
     }
 
-    global.store_profile.terminate();
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    
-    auto rate = duration != 0 ? (double)cnt / duration : 0;
-    spdlog::get("cads")->info("DB PROCESSING - CNT: {}, DUR: {}, RATE(ms):{} ", cnt, duration, rate);
-
-    // spdlog::get("cads")->info("Final Upload");
-    // http_post_whole_belt(revid, idx); // For replay and not having a complete belt, so something is uploaded
+    global.store_scan.terminate();
+    auto scan_filename = fmt::format("scan-{}.sqlite",global.scan_begin.time_since_epoch().count());
+    std::filesystem::remove(scan_filename);
     spdlog::get("cads")->info("Stopping save_send_thread");
   }
 }
