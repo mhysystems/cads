8631ee9b27fa772e42d03eac34ff38d079eeffe0
Jeff <jeff@hexagonhill.dev>
2023-05-03 16:20:00 +1000

    new Grafana table

---
+++ cads-gui/Migrations/20230503053602_NewTableGrafana.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cadsgui.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewTableGrafana : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Plots",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Url = table.Column<string>(type: "TEXT", nullable: false),
+                    Row = table.Column<int>(type: "INTEGER", nullable: false),
+                    Col = table.Column<int>(type: "INTEGER", nullable: false),
+                    Visible = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plots", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Plots");
+        }
+    }
+}
