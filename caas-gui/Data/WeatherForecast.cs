96c9f4d65195c288706f8ea698ba20ce50709c4f
Jeff <jeff@hexagonhill.dev>
2023-06-18 10:00:27 +1000

    init caas gui

---
+++ caas-gui/Data/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace caas_gui.Data;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
