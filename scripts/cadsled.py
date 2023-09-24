55bcbeec9d0b3759514b179bb5aaae098841194f
Jeff <jeff@hexagonhill.dev>
2023-09-24 21:57:39 +1000

    config fixes

--- scripts/cadsled.py
+++ scripts/cadsled.py
@@ -3,10 +3,12 @@ import Jetson.GPIO as GPIO
 import argparse
 
 def wireguard(up):
+    GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(21, GPIO.OUT)
     GPIO.output(21, GPIO.HIGH if up else GPIO.LOW)
-    GPIO.cleanup()
+    if not up:
+        GPIO.cleanup(21)
 
 parser = argparse.ArgumentParser(description='Control Cads LEDs')
 parser.add_argument("--wireguard", type=int, help="Wireguard Connection") 
