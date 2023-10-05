e5abf0842d7c8e873f0ef86be73ecaf5e0117668
Jeff <jeff@hexagonhill.dev>
2023-10-05 21:33:44 +1100

    develop cads led as service

--- scripts/cadsled.py
+++ scripts/cadsled.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import Jetson.GPIO as GPIO
 import argparse
+import signal
+
 
 def wireguard(up):
     GPIO.setwarnings(False)
@@ -10,10 +12,23 @@ def wireguard(up):
     if not up:
         GPIO.cleanup(21)
 
+def sigusr1(signum,frame):
+    wireguard(True)
+
+def sigusr2(signum,frame):
+    wireguard(False)
+
+
 parser = argparse.ArgumentParser(description='Control Cads LEDs')
 parser.add_argument("--wireguard", type=int, help="Wireguard Connection") 
 
 args = parser.parse_args()
+signal.signal(signal.SIGUSR1, sigusr1)
+signal.signal(signal.SIGUSR2, sigusr2)
 
 if args.wireguard is not None:
     wireguard(args.wireguard == 0)
+else:
+    while True:
+        signal.pause()
+
