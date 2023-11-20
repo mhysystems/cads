bed39346917122fb800c4c9b782f5e914ed2f025
Jeff <jeff@hexagonhill.dev>
2023-11-20 17:31:23 +1100

    raw dump.lua

---
+++ cads-edge/dump.lua
@@ -0,0 +1,129 @@
+json = require "json"
+
+gocatorFps = 984.0
+
+iirfilter = {
+  Skip = 10000,
+  Delay = 334,
+  Sos  = {{ 1.66008519e-07,  1.66008519e-07,  0.00000000e+00, 1.00000000e+00, -9.77361093e-01,  0.00000000e+00},
+          { 1.00000000e+00, -1.98638977e+00,  1.00000000e+00, 1.00000000e+00, -1.95693573e+00,  9.57429859e-01},
+          { 1.00000000e+00, -1.99649383e+00,  1.00000000e+00, 1.00000000e+00, -1.96256904e+00,  9.63016716e-01},
+          { 1.00000000e+00, -1.99836739e+00,  1.00000000e+00, 1.00000000e+00, -1.96958098e+00,  9.69971891e-01},
+          { 1.00000000e+00, -1.99901953e+00,  1.00000000e+00, 1.00000000e+00, -1.97638907e+00,  9.76726728e-01},
+          { 1.00000000e+00, -1.99931660e+00,  1.00000000e+00, 1.00000000e+00, -1.98230743e+00,  9.82601536e-01},
+          { 1.00000000e+00, -1.99947217e+00,  1.00000000e+00, 1.00000000e+00, -1.98725122e+00,  9.87512612e-01},
+          { 1.00000000e+00, -1.99955888e+00,  1.00000000e+00, 1.00000000e+00, -1.99139129e+00,  9.91629854e-01},
+          { 1.00000000e+00, -1.99960633e+00,  1.00000000e+00, 1.00000000e+00, -1.99496619e+00,  9.95190499e-01},
+          { 1.00000000e+00, -1.99962752e+00,  1.00000000e+00, 1.00000000e+00, -1.99821287e+00,  9.98430473e-01}}
+}
+
+conveyor = {
+  Id = 51,
+  Org  = "FMG",
+  Site = "FMG",
+  Name = "CV331",
+  Timezone = "Australia/Perth",
+  PulleyCircumference = 624 * math.pi,
+  TypicalSpeed = 2.63,
+  Belt =  50,
+  Length = 391960.0,
+  WidthN = 1200.0
+}
+
+belt = {
+  Id = 50,
+  Installed = "2023-01-14T00:00:00Z",
+  PulleyCover = 7.0,
+  CordDiameter = 0.0,
+  TopCover = 20.0,
+  Width = 1200.0,
+  Length = conveyor.Length,
+  LengthN = conveyor.TypicalSpeed / gocatorFps, 
+  Splices = 1,
+  Conveyor = 51
+}
+
+dbscan = {
+  InClusterRadius = 12,
+  MinPoints = 20
+}
+
+revolutionsensor = {
+  Source = "length",
+  TriggerDistance = conveyor.PulleyCircumference / 1,
+  Bias = 0,
+  Threshold = 0.05,
+  Bidirectional = false,
+  Skip = math.floor((conveyor.PulleyCircumference / (1000 * conveyor.TypicalSpeed)) * gocatorFps * 0.9)
+}
+
+y_res_mm = 1000 * conveyor.TypicalSpeed / gocatorFps -- In mm
+
+laserConf = {
+  Trim = true,
+  TypicalResolution = y_res_mm
+}
+
+measures = {
+  Enable = false
+}
+
+profileConfig = {
+  Width = belt.Width,
+  NaNPercentage = 0.15,
+  ClipHeight = 35.0,
+  PulleyEstimatorInit = -15.0,
+  IIRFilter = iirfilter,
+  PulleySamplesExtend = 10,
+  RevolutionSensor = revolutionsensor,
+  Conveyor = conveyor,
+  Dbscan = dbscan,
+  Measures = measures
+}
+
+function timeToString(time) -- overwritten externally
+  return tostring(time)
+end
+
+function main(sendmsg)
+
+  local gocator_cads = BlockingReaderWriterQueue()
+  local origin_savedb = BlockingReaderWriterQueue()
+  local savedb_luamain = BlockingReaderWriterQueue()
+  
+  local laser = gocator(laserConf,gocator_cads)
+
+  if laser == nil then
+    return
+  end
+
+  local belt_loop = loop_beltlength_thread(conveyor,gocator_cads,origin_savedb)
+  local thread_send_save = save_send_thread(conveyor,false,origin_savedb,savedb_luamain)
+
+  if laser:Start(gocatorFps) then
+    return
+  end
+
+  local beltprogress = 0
+
+  unloop = false
+  repeat
+    local is_value,msg_id,data = wait_for(savedb_luamain)
+
+    if is_value then
+      if msg_id == 2 then break 
+      elseif msg_id == 5 then break
+      elseif msg_id == 12 then
+        break
+      end
+    end
+
+    unloop = coroutine.yield(0)
+  until unloop
+
+  laser:Stop(true)
+  join_threads({belt_loop,thread_send_save})
+
+end
+
+mainco = coroutine.create(main)
